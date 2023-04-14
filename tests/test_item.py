8a7f7a0fcb1e6e8e9ff7938d0bbdbdc3a1a749c8
kuzhelga <kuzhelga81@gmail.com>
2023-04-14 20:20:03 +0300

    added tests for functions calculate_total_price and apply_discount

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,22 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+import pytest
+
+from src.item import Item
+
+
+@pytest.fixture
+def data():
+    """Экземпляр для теста"""
+    return Item("Laptop", 50000, 2)
+
+
+def test_calculate_total_price(data):
+    """Тест для проверки функции расчета стоимости товара"""
+    assert data.calculate_total_price() == 100000
+
+
+def test_apply_discount(data):
+    """Тест для проверки функции применения скидки"""
+    data.pay_rate = 0.7
+    data.apply_discount()
+    assert data.price == 35000
