de7b97a79aacd78601441866c895c446124dcf9c
kuzhelga <kuzhelga81@gmail.com>
2023-04-16 16:11:31 +0300

    added test_instantiate_from_csv

--- tests/test_item.py
+++ tests/test_item.py
@@ -20,3 +20,18 @@ def test_apply_discount(data):
     data.pay_rate = 0.7
     data.apply_discount()
     assert data.price == 35000
+
+
+def test_instantiate_from_csv():
+    """Тест для класс-метода, проверяет все ли товары загружены и совпадение с конкретным товаром"""
+    Item.instantiate_from_csv()
+    assert len(Item.all) == 5
+    item1 = Item.all[1]
+    assert item1.name == "Ноутбук"
+    assert item1.price == 1000
+    assert item1.quantity == 3
+
+def test_string_to_number():
+    """Тест для статического метода получения числа из строки"""
+    assert Item.string_to_number('10') == 10
+    assert Item.string_to_number('10.1') == 10
