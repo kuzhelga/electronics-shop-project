b8609c09648e717772a68b144837ce286ce9735d
kuzhelga <kuzhelga81@gmail.com>
2023-04-20 13:34:50 +0300

    added repr method

--- src/item.py
+++ src/item.py
@@ -22,7 +22,7 @@ class Item:
         Item.all.append(self)
 
     def __repr__(self):
-        return f'{self.__name} {self.price} {self.quantity}'
+        return f'Class {self.__class__.__name__}: {self.__name}, price {self.price}, quantity {self.quantity}'
 
     @property
     def name(self):
