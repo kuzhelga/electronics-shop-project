72b49361b71a94a52d79ff7b5d6dfbdf0f695a41
kuzhelga <kuzhelga81@gmail.com>
2023-05-11 15:50:13 +0300

    corrected class MixinLog

--- src/keyboard.py
+++ src/keyboard.py
@@ -3,25 +3,33 @@ from src.item import Item
 
 class MixinLog:
     """Класс для смены языка клавиатуры"""
-    def __init__(self):
-        self.__language == "EN"
+    __language = "EN"
+
+    def __init__(self, *args):
+        super().__init__(*args)
 
     @property
     def language(self):
         """Сеттер для раскладки языка"""
         return self.__language
 
+    @language.setter
+    def language(self, value):
+        """Сеттер для языка - выводит предупреждение при выборе неподходящего языка"""
+        raise AttributeError("property 'language' of 'KeyBoard' object has no setter")
+
     def change_lang(self):
         """Метод для смены раскладки клавиатуры"""
         if self.__language == "EN":
             self.__language = "RU"
-        else:
+        elif self.__language == "RU":
             self.__language = "EN"
+
         return self
+
 
 class KeyBoard(Item, MixinLog):
     """Класс для товара Клавиатура"""
 
-    def __init__(self, name, price, quantity):
-        super().__init__(name, price, quantity)
-
+    def __init__(self, *args):
+        super().__init__(*args)
